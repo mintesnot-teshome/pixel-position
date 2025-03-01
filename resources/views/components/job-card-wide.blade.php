1dae3ea3936a7898fae70929e5801044fcda8457
Mintesnot Teshome <mintesnotteshome0@gmail.com>
2025-03-01 23:18:32 +0300

    Refactor job card components to use a new panel component, enhance tag handling, and improve layout structure

--- resources/views/components/job-card-wide.blade.php
+++ resources/views/components/job-card-wide.blade.php
@@ -1,5 +1,4 @@
-<div class="p-4 bg-white/5 rounded-xl flex gap-x-6 border border-transparent hover:border-blue-600 group transition-colors duration-300">
-
+<x-panel class=" flex gap-x-6 ">
     <div>
  <x-employer-logo/>
 </div>
@@ -15,5 +14,4 @@
          <x-tag>Tag</x-tag>
         </div>
 
-
-</div>
+    </x-panel>
--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -1,4 +1,4 @@
-<div class="p-4 bg-white/5 rounded-xl flex flex-col text-center border border-transparent hover:border-blue-600 group transition-colors duration-300">
+<x-panel class=" flex flex-col text-center">
     <div class="self-start text-sm">Laracasts</div>
     <div class="py-6 ">
         <h3 class="group-hover:text-blue-600 text-xl font-bold transition-colors duration-300">Video Editor</h3>
@@ -6,10 +6,10 @@
     </div>
     <div class="flex justify-between item-center mt-auto">
         <div>
-         <x-tag>Tag</x-tag>
-         <x-tag>Tag</x-tag>
-         <x-tag>Tag</x-tag>
+         <x-tag size="small">Backend</x-tag>
+         <x-tag size="small">Frontend</x-tag>
+         <x-tag size="small">Manager</x-tag>
         </div>
     <x-employer-logo :width="42"/>
     </div>
-</div>
+</x-panel>
